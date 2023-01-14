8c9ce606847330fcc552d59f24298584a92f9729
villainscode <villainscode@gmail.com>
2023-01-14 18:45:54 +0900

    Initial commit

---
+++ src/test/java/com/example/springfirstrepo/SpringFirstRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springfirstrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFirstRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
